604a26628fe11c42dcbc952201a33c89cfbf2c81
Harout Mardirossian <harmardir@users.noreply.github.com>
2023-02-28 12:35:31 +0200

    user authentication

--- smartnotes/urls.py
+++ smartnotes/urls.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
-from django.urls import path
-from home import views
+from django.urls import path , include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home', views.home),
+    path('', include('home.urls')),
 ]
